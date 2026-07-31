a3679b02e75267a0128c85fa91037af346720e0f
panyan <maxwell2119@163.com>
2026-07-31 23:53:58 +0800

    重写 .gitignore：精简结构，添加 IDE/OS 忽略规则

---
+++ xiangxiang_double_pointer/trap.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def trap(self,n:int,height:List[int])->int:
+        ans=0
+
+        
+        return 0
+
+def main():
+    n=int(input())
+    height=list(map(int,input().split))
+    solution=Solution()
+    print(solution.trap(n,height))
+
+if __name__ == "__main__":
+    main()
